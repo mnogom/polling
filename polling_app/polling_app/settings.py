b64339359d88b7cd2025d17f6b7fe00d8d8ed5fa
Konstantin Freidlin <freidlinks@ya.ru>
2021-05-16 19:02:15 +0300

    Minor fixes

--- polling_app/polling_app/settings.py
+++ polling_app/polling_app/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from environs import Env
+
+
+#
+env = Env()
+env.read_env(override=True)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a)^4q*za*b1sh_dhm5$#y8g+3-%%%qnzw!x6k07)0t+0kd675g'
+# SECRET_KEY = 'a)^4q*za*b1sh_dhm5$#y8g+3-%%%qnzw!x6k07)0t+0kd675g'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
